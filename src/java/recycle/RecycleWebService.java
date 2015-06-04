1693f0a0a1b4e5125784821f9f3bc2550d91f46c
hoffmann1 <morten.h.christensen@gmail.com>
2015-06-04 21:20:41 +0200

    comments added

--- src/java/recycle/RecycleWebService.java
+++ src/java/recycle/RecycleWebService.java
@@ -30,6 +30,15 @@ public class RecycleWebService
 {
     private String serverURL = "http://vmi19372.iry.dk:8880/RecycleWebService/images/";  
     
+    
+    
+       /**
+    * Creates a new User in the database. 
+    * 
+    * @param  user a newly created User object, not including the id.
+    * @return      the id of the newly created User object, or -1 if there was an error.
+    * @see         User
+    */
     @WebMethod(operationName = "createUser")
     public long createUser(@WebParam(name = "user")User user)
     {
@@ -51,6 +60,14 @@ public class RecycleWebService
         return returnId;
     }
     
+           /**
+     * returns a User object from the database based on id. 
+     * 
+     * @param  id   the id of user one wishes to retrieve
+     * @return      the specified user if one exists, otherwise it returns null
+     * @see         User
+     */
+    
     @WebMethod(operationName = "getUser")
     public User getUser(@WebParam(name = "id")long id)
     {
@@ -69,6 +86,12 @@ public class RecycleWebService
         return null;
     }
     
+       /**
+    * Updates a User object in the database. 
+    * 
+    * @param  user a User object containing the changes to be updated, remember the id.
+    * @see         User
+    */
     @WebMethod(operationName = "updateUser")
     public void updateUser(@WebParam(name = "User")User user)
     {
@@ -85,6 +108,15 @@ public class RecycleWebService
             Logger.getLogger(RecycleWebService.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
+    
+           /**
+     * validates User credentials against the database. 
+     * 
+     * @param  username a String containing the username.
+     * @param  password a String containing the password.
+     * @return      if the user is validated the User object will be returned.
+     * @see         User
+     */
     
     @WebMethod(operationName = "validateUser")
     public User validateUser(@WebParam(name = "username")String username,@WebParam(name = "password") String password)
@@ -104,6 +136,13 @@ public class RecycleWebService
         return null;        
     }
     
+              /**
+     * Creates a new Publication object in the database. 
+     * 
+     * @param  publication a Publication object containing the newly created publication.
+     * @return      the id of the newly created Publication, or -1 if there was an error.
+     * @see         Publication
+     */
     @WebMethod(operationName = "createPublication")
     public long createPublication(@WebParam(name = "publication")Publication publication)
     {
@@ -126,6 +165,14 @@ public class RecycleWebService
         return returnId;
     }
     
+              /**
+    * returns a Publication object from the database based on id. 
+    * 
+    * @param  id   the id of the publication one wishes to retrieve
+    * @return      the specified publication if one exists, otherwise it returns null
+    * @see         Publication
+    */
+    
     @WebMethod(operationName = "getPublication")
     public Publication getPublication(@WebParam(name = "id")long id)
     {
@@ -143,6 +190,14 @@ public class RecycleWebService
         }
         return null;
     }
+    
+              /**
+    * Returns a list of all Publication objects from the database in descending order. 
+    * 
+    * @return      a list of all Publication objects from the database in descending order.
+    * @see         Publication
+    */
+    
     @WebMethod(operationName = "getAllPublications")
     public Collection<Publication> getAllPublications()
     {
@@ -160,6 +215,14 @@ public class RecycleWebService
         }
         return null;
     }
+    
+              /**
+    * Returns a list of all Subscriptions associated with the publication id specified. 
+    * 
+    * @param publicationId the id of the publication.
+    * @return      a list of all subscription objects associated with the publication.
+    * @see         Subscription
+    */
     
     @WebMethod(operationName = "getPublicationSubscriptions")
     public Collection<Subscription> getPublicationSubscriptions(@WebParam(name = "publicationId")long publicationId)
@@ -179,6 +242,14 @@ public class RecycleWebService
         return null;
     }
     
+                  /**
+    * Returns a list of all Subscriptions associated with the user id specified. 
+    * 
+    * @param userId the id of the user.
+    * @return      a list of all subscription objects associated with the user.
+    * @see         Subscription
+    */
+    
     @WebMethod(operationName = "getUserSubscriptions")
     public Collection<Subscription> getUserSubscriptions(@WebParam(name = "userId")long userId)
     {
@@ -197,6 +268,14 @@ public class RecycleWebService
         return null;
     }
     
+                 /**
+    * Creates a new Subscription object in the database. 
+    * 
+    * @param  userId the id of the user associated with this subscription.
+    * @param  publicationId the id of the publication associated with this subscription.
+    * @see         Subscription
+    */
+    
     @WebMethod(operationName = "createSubscription")
     public void createSubscription(@WebParam(name = "userId")long userId,@WebParam(name = "publicationId") long publicationId)
     {
@@ -212,6 +291,16 @@ public class RecycleWebService
             Logger.getLogger(RecycleWebService.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
+    
+                 /**
+    * Saves a Publication image onto the server, and assigns a unique name for the image
+    * 
+    * @param  filename the name of the file.
+    * @param  filedata a bytearray containing the image data.
+    * @param  publicationId the id of the publication.
+    * @return a String containing the url for the image on the server.
+    * @see         Publication
+    */
     
     @WebMethod(operationName = "saveImage")
     public String saveImage(@WebParam(name = "filename")String filename,@WebParam(name = "filedata") byte[] filedata,@WebParam(name = "publicationId")long publicationId)
@@ -260,6 +349,13 @@ public class RecycleWebService
         
         return returnURL;
     }
+    
+                  /**
+    * Returns a list of all Category objects from the database. 
+    * 
+    * @return      a Collection of all Category objects from the database, or null if none exists.
+    * @see         Category
+    */
     
     @WebMethod(operationName = "getAllCategories")
     public Collection<Category> getAllCategories()
